ba09934aed779398f21a593a730f0094072ce0d5
Marco Tran <mtran0132@gmail.com>
2018-10-10 22:53:18 -0700

    tried my hardest

---
+++ OriginalHeapster.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef __ORIGINALHEAPSTER_H__
+#define __ORIGINALHEAPSTER_H__
+
+namespace OriginalHeapster
+
+{
+	void my_initialize_heap(int size);
+	void my_alloc(int size);
+	void my_free(void *data);
+
+};
+
+#endif
